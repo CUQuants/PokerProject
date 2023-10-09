9186cf6d54f4c1bdb5206dbd9821b9d9018feb3d
Andrew Fox <apfox500@gmail.com>
2023-10-09 10:26:25 -0600

    Added directory and changed data types

--- cardDeck.py
+++ cardDeck.py
@@ -1,10 +1,12 @@
 from card import Card
+from suit import Suit
 import random
+
 
 class CardDeck:
     def __init__(self):
-        self.ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-        self.suits = ['Spades', 'Clubs', 'Diamonds', 'Hearts']
+        self.ranks = range(2,14)
+        self.suits = [e.value for e in Suit]
         self.cards = [Card(rank, suit) for rank in self.ranks for suit in self.suits]
 
     def shuffle(self):
